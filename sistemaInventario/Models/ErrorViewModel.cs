3f94d7e64e955656206c0e375808570193eb7394
Eduar Garcia <eduarfrancis11@gmail.com>
2024-01-25 10:19:43 -0300

    iniciar proyecto

---
+++ sistemaInventario/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sistemaInventario.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
