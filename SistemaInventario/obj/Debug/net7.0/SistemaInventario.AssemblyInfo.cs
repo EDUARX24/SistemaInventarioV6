68224d73d4a0fff5c93c2492023bae8590c24b48
Eduar Garcia <eduarfrancis11@gmail.com>
2024-05-03 23:21:58 -0600

    Configuracion del proyecto

--- SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2bba9ea1d949dfb5ed5787b43126d970365ba17")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
